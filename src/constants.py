5b60e707b2c043773c70ae363e396eccc6747b96
OctaGirardelli <ogirardelli@gmail.com>
2025-04-19 14:58:47 -0300

    Agrega ejercicio 1 seccion A

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+# Ruta base del proyecto
+ROOT_DIRECTORY = Path(__file__).resolve().parent.parent
+
+# Ruta de la carpeta 'files'
+FILES_DIRECTORY = ROOT_DIRECTORY / 'files'
